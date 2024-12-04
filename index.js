e58e7e961cee07adb730c43cec5eeebe101a8aae
Ashiqur1819 <ashiqurrahman1819@gmail.com>
2024-12-04 21:27:39 +0600

    Added delete operation for movies

--- index.js
+++ index.js
@@ -49,6 +49,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/movies/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await movieCollection.deleteOne(query);
+      res.send(result);
+    });
+
 
 
     // Send a ping to confirm a successful connection
