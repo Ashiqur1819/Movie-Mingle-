feb3810f659f15fd6ff9cc773c161d9ecb21f862
Ashiqur1819 <ashiqurrahman1819@gmail.com>
2024-12-06 23:03:54 +0600

    Deploy the project

--- index.js
+++ index.js
@@ -23,9 +23,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
-    
+
     const database = client.db("MoviesDB");
     const movieCollection = database.collection("movies");
     const favouriteCollection = database.collection("favourites");
@@ -124,16 +122,8 @@ async function run() {
               res.send(result);
             });
 
-
-
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
   } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
+
   }
 }
 run().catch(console.dir);
